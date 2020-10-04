8a182a92b974019be34aeec8ce6429026a03a324
GenaKashin <gengambit37@gmail.com>
2020-10-04 16:50:46 +0300

    Updates

---
+++ precache-manifest.71536d9d88aa2564720e314846ea4364.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f37ecdafe3cd07f007d8fea4c99a072",
+    "url": "/photolist/index.html"
+  },
+  {
+    "revision": "0290b3f1bc92b15fde22",
+    "url": "/photolist/static/css/main.5d1e9691.chunk.css"
+  },
+  {
+    "revision": "aabf632950d832ea1077",
+    "url": "/photolist/static/js/2.1e95fe64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/photolist/static/js/2.1e95fe64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0290b3f1bc92b15fde22",
+    "url": "/photolist/static/js/main.5a93ebc8.chunk.js"
+  },
+  {
+    "revision": "a8b3a7fde0bf2b0d1e4c",
+    "url": "/photolist/static/js/runtime-main.c942c68c.js"
+  }
+]);
